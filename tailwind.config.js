af4e0517606d4af52fbb0e5947422668989edca2
Yusuf Khasbulatov <khashashin@gmail.com>
2022-10-19 18:41:43 +0200

    fix tailwind compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/app/**/*.html'],
+  content: ['./src/**/*.{html,js,ts}', './src/app/**/*.{html,js,ts}'],
   theme: {
     extend: {},
   },
